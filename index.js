62d79adc5659ff656451f7e12e505a26de0d4c5a
pratamafernanda68 <pratamafernanda68@gmail.com>
2026-04-15 09:03:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-"use strict";
-let nama = "Budi";
-let umur = 17;
-let aktif = true;
+var nama = "Budi";
+var umur = 17;
+var aktif = true;
 console.log(nama, umur, aktif);
+function login(username, password) {
+    return username === "admin" && password === "admin";
+}
+console.log(login("admin", "admin"));
+var user = {
+    id: 1,
+    nama: "Sinta",
+    role: "admin"
+};
+console.log(user);
+var buku1 = {
+    id: 1,
+    judul: "Belajar Backend"
+};
+console.log(buku1);
