5e9fa4ebaceadd457da63272e0ff125dcf85f5b2
pratamafernanda68 <pratamafernanda68@gmail.com>
2026-04-15 08:21:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let nama = "Budi";
+let umur = 17;
+let aktif = true;
+console.log(nama, umur, aktif);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let nama: string = "Budi"; 
+let umur: number = 17; 
+let aktif: boolean = true; 
+console.log(nama, umur, aktif); 
